0abc46f596c2e92b0127f70b41e0de0b8a56d0aa
Andrew-Stuber <andrewstuber20@gmail.com>
2023-12-07 01:04:50 +0700

    rollback status from payment service

--- create-order/src/main/java/com/createorder/createorder/service/MessageSubscriber.java
+++ create-order/src/main/java/com/createorder/createorder/service/MessageSubscriber.java
@@ -2,10 +2,15 @@ package com.createorder.createorder.service;
 
 import org.springframework.stereotype.Service;
 
+import com.createorder.createorder.controller.OrderController;
 import com.createorder.createorder.model.Order;
 import com.createorder.createorder.model.OrderStatus;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.micrometer.core.instrument.MeterRegistry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
@@ -17,10 +22,14 @@ public class MessageSubscriber implements MessageListener {
     OrderService orderService;
 
     private final ObjectMapper objectMapper;
+    private final MeterRegistry registry;
 
-    public MessageSubscriber(ObjectMapper objectMapper) {
+    public MessageSubscriber(ObjectMapper objectMapper, MeterRegistry registry) {
         this.objectMapper = objectMapper;
+        this.registry = registry;
     }
+
+    private final Logger LOG = LoggerFactory.getLogger(OrderController.class);
 
     public void onMessage(Message message, byte[] pattern) {
         String channel = new String(pattern);
@@ -43,13 +52,30 @@ public class MessageSubscriber implements MessageListener {
             String id = order.getId();
             OrderStatus status = order.getStatus();
 
+            LOG.info("order " + id + " is successfully delivered");
+            registry.counter("completed.orders.total").increment();
+            
             orderService.changeStatus(id, status);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    // Recieve rollback message from payment service
     private void paymentRollback(String message) {
         System.out.println("Message from payment service: " + message);
+
+        try {
+            Order order = objectMapper.readValue(message, Order.class);
+            String id = order.getId();
+            OrderStatus status = order.getStatus();
+
+            LOG.error("Rollback from payment service for order " + id + " with error status " + status);
+            registry.counter("error.orders.total").increment();
+
+            orderService.changeStatus(id, status);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
